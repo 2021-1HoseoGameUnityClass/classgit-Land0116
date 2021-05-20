4e84e81fbcd9b57d76c1c84fb6cb0926cfb876dd
20201287강민혁 <onueer@gmail.com>
2021-05-20 15:19:42 +0900

    12주차

--- Testa/Assets/Scripts/Player.cs
+++ Testa/Assets/Scripts/Player.cs
@@ -6,10 +6,25 @@ public class Player : MonoBehaviour
 {
     public float moveSpeed = 0.1f;
 
+    public float JumpForce = 0.1f;
+    public bool Jump = true;
+    public GameObject bulletObj = null;
+    public GameObject InstantiateObj = null;
+
     // Update is called once per frame
     void Update()
     {
         Move();
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            PlayerJump();
+        }
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shot();
+        }
     }
 
     public void Move()
@@ -36,5 +51,34 @@ public class Player : MonoBehaviour
         {
             GetComponent<Animator>().SetBool("IsMove", false);
         }
+    }
+
+    public void PlayerJump()
+    {
+        if (Jump == false)
+        {
+            GetComponent<Animator>().SetBool("IsMove", false);
+            GetComponent<Animator>().SetBool("IsJump", true);
+
+            Vector2 vector2 = new Vector2(0, JumpForce);
+            GetComponent<Rigidbody2D>().AddForce(vector2);
+            Jump = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Platform")
+        {
+            GetComponent<Animator>().SetBool("IsJump", false);
+            Jump = false;
+        }
+    }
+
+    public void Shot()
+    {
+        float direction = transform.localScale.x;
+        Quaternion quaternion = new Quaternion(0, 0, 0, 0);
+        Instantiate(bulletObj, InstantiateObj.transform.position, quaternion).GetComponent<Bullet>().InstantiateBullet(direction);
     }
 }
